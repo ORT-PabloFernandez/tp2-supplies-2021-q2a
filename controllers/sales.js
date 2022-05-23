2c686b633531f7e85144bd484e04c59e0cb901a6
Juan Pablo Forno <fornojp@gmail.com>
2022-05-23 01:21:33 -0300

    Juan Pablo Forno

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,7 +1,29 @@
-const sales = require('../data/supplies');
+const sales = require("../data/supplies");
 
-async function getSales(){    
-    return sales.getAllSales();
+async function getSales() {
+  return sales.getAllSales();
+}
+async function getVentaId(id) {
+  return sales.getVentaId(id);
+}
+async function getPorVenta(venta) {
+  return sales.getPorVenta(venta);
+}
+async function getPorEmail(email) {
+  return sales.getPorEmail(email);
+}
+async function getInsatisfechos() {
+  return sales.getInsatisfechos();
+}
+async function getLocalidad(localidad) {
+  return sales.getLocalidad(localidad);
 }
 
-module.exports = {getSales};
+module.exports = {
+  getSales,
+  getVentaId,
+  getPorVenta,
+  getPorEmail,
+  getInsatisfechos,
+  getLocalidad,
+};
